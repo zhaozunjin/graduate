8b1a022da48bd3e3c6de16fe057f829320663db5
zhaozunjin <1763020129@qq.com>
2018-04-25 21:53:07 +0800

    k近邻分类算法实现

---
+++ kNN.py
@@ -0,0 +1,127 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on Sep 16, 2010
+kNN: k Nearest Neighbors
+
+Input:      inX: vector to compare to existing dataset (1xN)
+            dataSet: size m data set of known vectors (NxM)
+            labels: data set labels (1xM vector)
+            k: number of neighbors to use for comparison (should be an odd number)
+            
+Output:     the most popular class label
+
+@author: pbharrin
+'''
+from numpy import *
+import operator
+from os import listdir
+'''
+函数名称：分类函数
+功能：将数据集进行分类
+参数：inx，数据集，标签，k值
+'''
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()     
+    classCount={}          
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+def createDataSet():
+    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    labels = ['A','A','B','B']
+    return group, labels
+'''
+函数名称：file2matrix
+函数功能：将文件格式转化为矩阵
+'''
+def file2matrix(filename):
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())         #get the number of lines in the file
+    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
+    classLabelVector = []                       #prepare labels return   
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+'''
+函数名称：autoNorm
+函数功能：归一化数据
+''' 
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
+    return normDataSet, ranges, minVals
+  
+'''
+函数名称：datingClassTest
+函数功能：评估测试器分类效果
+'''  
+def datingClassTest():
+    hoRatio = 0.50      #hold out 10%
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')       #load data setfrom file
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print "the total error rate is: %f" % (errorCount/float(numTestVecs))
+    print errorCount
+    
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')           #load the training set，得到了文件的名字列表，如 0_13.txt
+    m = len(trainingFileList)          #m=样本的个数，m为numpy矩阵提供了行数，一行即为一个样本（1X1024）
+    trainingMat = zeros((m,1024))     #初始化numpy矩阵
+    for i in range(m):				 
+        fileNameStr = trainingFileList[i]  #得到了单个训练文件的名字
+        fileStr = fileNameStr.split('.')[0]     #take off .txt,去掉后缀，取出字符串
+        classNumStr = int(fileStr.split('_')[0])#去掉 ‘_’后的内容，就可以得出训练集的真实样本值
+        hwLabels.append(classNumStr)		#将样本值设定为标签，存在数组中
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)  #调用img2vector函数，将图像转换为向量，进而组成训练集矩阵
+    testFileList = listdir('testDigits')        #iterate through the test set
+    errorCount = 0.0   #计数器，用来统计分类错误次数
+    mTest = len(testFileList)  #测试集样本个数
+    for i in range(mTest):   
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print "\nthe total number of errors is: %d" % errorCount
+    print "\nthe total error rate is: %f" % (errorCount/float(mTest))
+	print "test..."
+
+if __name__ == "__main__":
+    handwritingClassTest
